4c3fd49296e242b7aaa956631e9253f0cc1546aa
yuhongda <yuhongda@rongcloud.cn>
2019-04-23 18:08:28 +0800

    fix: 修改错误描述

--- src/error.js
+++ src/error.js
@@ -3,147 +3,147 @@ let getErrors = () => {
   let errors = [{
     code: 10000,
     name: 'INSTANCE_IS_DESTROYED',
-    msg: 'RongRTC 实例已销毁，请重新创建实例'
+    msg: 'RongRTC instance has been destroyed'
   },
   {
     code: 50000,
     name: 'IM_NOT_CONNECTED',
-    msg: '请在 IM 连接成功后开始音频业务'
+    msg: 'IM not connected'
   },
   {
     code: 50001,
     name: 'ROOM_ID_IS_ILLEGAL',
-    msg: '房间号不合法，只能包含大小写字母、阿拉伯数字、+、=、-、_ 且长度不能超过 64 个字符'
+    msg: 'The roomId is illegal and can contain only upper and lower case letters, Arabic numerals, +, =, -, _ and cannot exceed 64 characters in length'
   },
   {
     code: 50002,
     name: 'ROOM_REPEAT_JOIN',
-    msg: '重复加入房间'
+    msg: 'Not rejoin the room'
   },
   {
     code: 50010,
     name: '',
-    msg: 'http 请求超时'
+    msg: 'Http request timeout'
   },
   {
     code: 50011,
     name: '',
-    msg: 'http response 异常（404、500）'
+    msg: 'http response error'
   },
   {
     code: 50012,
     name: '',
-    msg: '请求未发出去、断网'
+    msg: 'Network unavailable'
   },
   {
     code: 50020,
     name: '',
-    msg: '资源已发布'
+    msg: 'Resources has been published'
   },
   {
     code: 50021,
     name: 'SET_OFFER_ERROR',
-    msg: '设置 Offer 错误'
+    msg: 'Set offer error'
   },
   {
     code: 50021,
     name: 'SET_ANSWER_ERROR',
-    msg: '设置 Answer 错误'
+    msg: 'Set answer error'
   },
   {
     code: 50023,
     name: 'PUBLISH_STREAM_EXCEED_LIMIT',
-    msg: '发布资源个数已经到达上限'
+    msg: 'The maximum number of published resources has been reached'
   },
   {
     code: 50024,
     name: 'STREAM_NOT_EXIST',
-    msg: 'stream 不存在，请检查传入参数, id、stream.type、stream.tag 是否正确'
+    msg: 'Stream not exist. Please check user.id、stream.type or stream.tag'
   },
   {
     code: 50030,
     name: 'SUBSCRIBE_STREAM_NOT_EXIST',
-    msg: '订阅不存在的资源'
+    msg: 'Subscribe to non-existent resource'
   },
   {
     code: 50030,
     name: 'STREAM_TRACK_NOT_EXIST',
-    msg: 'Track 不存在，请检查传入参数 stream.type 是否正确'
+    msg: 'Track not exist. Please check user.id、stream.type or stream.tag'
   },
   {
     code: 50031,
     name: 'STREAM_SUBSCRIBED',
-    msg: '资源已订阅'
+    msg: 'Resources has been subscribed'
   },
   {
     code: 50032,
     name: 'UNSUBSCRIBE_STREAM_NOT_EXIST',
-    msg: '取消订阅不存在资源'
+    msg: 'Unsubscribe to non-existent resource'
   },
   {
     code: 50050,
     name: 'RTC_NOT_JOIN_ROOM',
-    msg: '未加入房间，加入成功后方可调用业务方法'
+    msg: 'Please join the room first'
   },
   {
     code: 50051,
     name: 'SOCKET_UNAVAILABLE',
-    msg: 'IM Socket 连接不可用'
+    msg: 'IM socket unavailable'
   },
   {
     code: 50052,
     name: 'NETWORK_UNAVAILABLE',
-    msg: '网络不可用'
+    msg: 'Network unavailable'
   },
   {
     code: 50053,
     name: 'IM_SDK_VER_NOT_MATCH',
-    msg: 'IM SDK 版本过低，最低版本 2.4.0，详细请参考: https://www.rongcloud.cn/docs/web_rtclib.html'
+    msg: 'IM SDK version is too low, minimum version 2.4.0, please check: https://www.rongcloud.cn/docs/web_rtclib.html'
   },
   {
     code: 50054,
     name: 'STREAM_DESKTOPID_ILLEGAL',
-    msg: '获取屏幕共享流失败，desktopStreamId 非法'
+    msg: 'Failed to get screen shared stream, illegal desktopStreamId'
   },
   {
     code: 50055,
     name: 'PARAMTER_ILLEGAL',
-    msg: '请检查参数，{name} 参数为必传入项'
+    msg: 'Please check the parameters, the {name} parameter is mandatory'
   },
   {
     code: 50056,
     name: 'ENGINE_ERROR',
-    msg: '音视频引擎不正确'
+    msg: 'RTC engine error'
   },
   {
     code: 50057,
     name: 'MEDIA_SERVER_ERROR',
-    msg: '当前网络异常，或 Media Server 不可用'
+    msg: 'Network is abnormal or Media Server is unavailable'
   },
   {
     code: 50058,
     name: 'MEDIA_SERVER_RESPONSE_EMPTY',
-    msg: 'Media Server Response Body 为空'
+    msg: 'Media Server response body is empty'
   }, {
     code: 40001,
     name: 'NOT_IN_ROOM',
-    msg: '当前用户不在房间内'
+    msg: 'Not in the room'
   }, {
     code: 40002,
     name: 'INTERNAL_ERROR',
-    msg: 'IM Server 内部错误'
+    msg: 'IM Server internal error'
   }, {
     code: 40003,
     name: 'HAS_NO_ROOM',
-    msg: 'IM Server 房间信息不存在'
+    msg: 'IM Server room info not exist'
   }, {
     code: 40004,
     name: 'INVALID_USERID',
-    msg: 'userId 不合法'
+    msg: 'UserId illegal'
   }, {
     code: 40005,
     name: 'REPEAT_JOIN_ROOM',
-    msg: '重复加入房间'
+    msg: 'Not rejoin the room'
   }];
 
   let errorMap = {
